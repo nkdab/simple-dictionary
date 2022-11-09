4b6ec76dc9428299d09f2463490725de6c578ff9
a.ivanov <a.ivanov@sprintf.ru>
2022-11-09 23:15:56 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { computed, ref } from "vue";
+import fuzzySort from "fuzzysort";
+import dictionary from "@/constants/dictionary";
+import Autocomplete from "vue3-autocomplete"
+
+const selected = ref(null);
+const query = ref("");
+
+const results = computed(() => {
+  return fuzzySort
+    .go(query.value, dictionary, { keys: ["ru", "en", "other"] })
+    .map((i) => i.obj);
+});
+</script>
+
+<template>
+  <main class="content">
+    <Autocomplete
+      @input="query = $event"
+      @onSelect="selected = $event"
+      :display-item="(item) => item.en + ' / ' + item.ru"
+      :results="results"
+      :input-class="['text-black w-full rounded']"
+      :results-container-class="['absolute bg-white text-black']"
+      :results-item-class="['cursor-pointer hover:bg-gray-100']"
+    />
+    <div class="results mt-4 text-gray-900 p-2 bg-orange-200 w-full">
+      <div
+        v-if="selected && selected.en"
+      >
+        <div v-if="selected.ru">
+          <h1 class="font-medium text-lg">Русский</h1>
+          <p>{{ selected.ru }} {{ selected.acronym && `(${selected.acronym})` }}</p>
+        </div>
+        <div>
+          <div class="w-full bg-gray-500 h-[1px] my-4"></div>
+          <h1 class="text-lg font-medium">English</h1>
+          <p>{{ selected.en }}</p>
+        </div>
+        <div v-if="selected.other">
+          <div class="w-full bg-gray-500 h-[1px] my-4"></div>
+          <h1 class="text-lg font-medium">Другое</h1>
+          <p>{{ selected.other }}</p>
+        </div>
+      </div>
+      <div v-else class="text-center">Ничего не найдено...</div>
+    </div>
+  </main>
+</template>
+
+<style></style>
